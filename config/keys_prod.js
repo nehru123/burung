25928ea77c0633a102e2ada77cb4b3f0b3fa96cc
nehru123 <nehrudkidz@gmail.com>
2020-04-27 22:32:04 +0700

    make it

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,10 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://ternakadmin:ternakan@ternakdb-gx3sw.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret",
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
 };
-
-// module.exports = {
-//   mongoURI: process.env.MONGO_URI,
-//   secretOrKey: process.env.SECRET_OR_KEY
-// }
