266abe2e54cdedb328e0a39d4f8e2c8b50dd25ae
nehru123 <nehrudkidz@gmail.com>
2020-04-27 08:52:52 +0700

    make it better

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,8 +1,7 @@
 module.exports = {
   mongoURI:
-    // "mongodb+srv://kenaridb:kenariku123@kenaridb-6pm3p.mongodb.net/test?retryWrites=true&w=majority",
     // "mongodb+srv://ptpnxdjoembangbaru:djoembangbaruptpnx@ptpnx-djoembang-d15dj.mongodb.net/test?retryWrites=true&w=majority",
     // "mongodb+srv://panjigemilang:xtremesuper98@devconnector-vasnt.mongodb.net/test?retryWrites=true&w=majority",
     "mongodb+srv://ternakadmin:ternakan@ternakdb-gx3sw.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
+  secretOrKey: "secret",
 };
