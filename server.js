e0a12cde84d4dd2ad8a8063f7c7b3da06f00b9bc
nehru123 <nehrudkidz@gmail.com>
2020-04-27 13:10:19 +0700

    make it betterrer

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ require("./config/passport")(passport);
 // use routes
 app.use("/api/users", user);
 app.use("/api/burung", burung);
-app.use("/api/gallery", gallery);
+app.use("/api/Gallery", gallery);
 app.use("/api/report", report);
 app.use("/api/finance", finance);
 app.use("/api/breeding", breeding);
