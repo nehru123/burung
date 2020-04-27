86f86b1b01af2fe49d4f82a1eca394075ae3166d
nehru123 <nehrudkidz@gmail.com>
2020-04-27 21:38:27 +0700

    make it

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
-}
+  mongoURI:
+    "mongodb+srv://ternakadmin:ternakan@ternakdb-gx3sw.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
