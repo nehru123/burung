ae221fe8f7e1b8fc3b8e9791dbc20ac257ddc641
nehru123 <nehrudkidz@gmail.com>
2020-04-30 22:52:01 +0700

    fixxxx

--- client/src/components/utils/ServicesBatch.js
+++ client/src/components/utils/ServicesBatch.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
   baseURL: "/api/batch",
 });
 export const insertBatch = (payload) => api.post(`/add`, payload);
@@ -10,16 +9,7 @@ export const getName = (id, payload) => api.get(`/getname/${id}`);
 export const updateBirdById = (id, payload) =>
   api.put(`/update/${id}`, payload);
 export const deleteBirdById = (id) => api.delete(`/delete/${id}`);
-=======
-    baseURL: '/api/batch',
-})
 
-export const insertBatch = payload => api.post(`/add`, payload)
-export const getBatchById = (id, payload) => api.get(`/getBatch/${id}`)
-export const getName = (id, payload) => api.get(`/getname/${id}`)
-export const updateBirdById = (id, payload) => api.put(`/update/${id}`, payload)
-export const deleteBirdById = id => api.delete(`/delete/${id}`)
->>>>>>> b000c7cc387f06065e903ca3cf703dceabb9b4ca
 const apis = {
   insertBatch,
   getBatchById,
