b9e51a4ae8f75279591e1e90ab853c76a9c33d59
nehru123 <nehrudkidz@gmail.com>
2020-04-30 10:18:05 +0700

    make it

--- routes/api/image.js
+++ routes/api/image.js
@@ -5,7 +5,7 @@ const Image = require("../../models/Image");
 
 // get
 router.get("/getImage/:id", (req, res) => {
-  Image.find({judul: req.params.id}).then(Images => {
+  Image.find({ judul: req.params.id }).then((Images) => {
     return res.status(200).json({ success: true, data: Images });
   });
 });
@@ -14,18 +14,17 @@ router.get("/getImage/:id", (req, res) => {
 router.post("/add", (req, res) => {
   const newImage = new Image({
     judul: req.body.judul,
-    image: req.body.image
+    image: req.body.image,
   });
-  newImage.save().then(Images => res.json(Images));
+  newImage.save().then((Images) => res.json(Images));
 });
 
 //update status burung
 
-
 router.delete("/delete/:id", (req, res) => {
   Image.findById(req.params.id)
-    .then(Images => Images.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+    .then((Images) => Images.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
