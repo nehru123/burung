87206f39f06b67549dbd195a528ab3009ef5ed18
nehru123 <nehrudkidz@gmail.com>
2020-05-06 22:56:14 +0700

    baru

--- client/src/components/pages/LaporanKeuangan.js
+++ client/src/components/pages/LaporanKeuangan.js
@@ -515,7 +515,6 @@ export default class LaporanKeuangan extends Component {
                 <th scope="col">Bulan</th>
                 <th scope="col">Pemasukan</th>
                 <th scope="col">Pengeluaran</th>
-                <th scope="col">Selisih</th>
 
                 <th scope="col">Action</th>
               </tr>
@@ -530,7 +529,6 @@ export default class LaporanKeuangan extends Component {
                       </td>
                       <td>{fil.pendapatan}</td>
                       <td>{fil.pengeluaran}</td>
-                      <td>{fil.pendapatan - fil.pengeluaran}</td>
                       <td>
                         <Link
                           to={
